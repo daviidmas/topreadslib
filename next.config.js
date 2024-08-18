1e9501d5fa22199676a46804fb11a74b6225382d
daviidx23 <davidmas.sm@gmail.com>
2024-08-19 00:35:49 +0200

    cloudflare pages test

--- next.config.js
+++ next.config.js
@@ -1,8 +1,15 @@
+import { setupDevPlatform } from '@cloudflare/next-on-pages/next-dev';
+
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
   images : { 
     domains : ['upload.wikimedia.org', 'en.wikipedia.org', "images.unsplash.com"]
   }
 };
+
+if (process.env.NODE_ENV === 'development') {
+  await setupDevPlatform();
+}
 
 module.exports = nextConfig;
